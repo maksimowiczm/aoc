7862c8d6677b68bbfd7e13a7d6192eff9af42d5b
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-12-01 17:35:08 +0100

    Day 1 Part 1 tests

--- day01/tests/tests.cpp
+++ day01/tests/tests.cpp
@@ -1,16 +1,34 @@
-extern "C"{
+extern "C" {
 #include "day01/lib.h"
 }
 
 #include "gtest/gtest.h"
 
-TEST(DAY01_TESTS, test){
+TEST(DAY01_TESTS, trash) {
+  const std::string input = "abc\n";
+
+  const auto output = day01_result(input.c_str(), input.length());
+
+  EXPECT_EQ(output, 0ull);
+}
+
+TEST(DAY01_TESTS, example_01) {
   const std::string input = "1abc2\n"
-                    "pqr3stu8vwx\n"
-                    "a1b2c3d4e5f\n"
-                    "treb7uchet";
+                            "pqr3stu8vwx\n"
+                            "a1b2c3d4e5f\n"
+                            "treb7uchet";
 
   const auto output = day01_result(input.c_str(), input.length());
 
   EXPECT_EQ(output, 142ull);
+}
+
+#include "input_01.hpp"
+
+TEST(DAY01_TESTS, puzzle_01) {
+  const std::string input(INPUT);
+
+  const auto output = day01_result(input.c_str(), input.length());
+
+  EXPECT_EQ(output, 56108ull);
 }
