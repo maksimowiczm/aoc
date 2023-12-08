ac1277fd32aa0b07974bc4fb85da76397cad86bf
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-12-08 17:51:48 +0100

    Day 8 solution 2

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -1,4 +1,5 @@
 use crate::node::Node;
+use num_integer::Integer;
 use std::{collections::HashMap, ops::ControlFlow};
 
 mod node;
@@ -52,4 +53,116 @@ pub fn solution_01(input: &str) -> u64 {
         ControlFlow::Break(Some((_, _, result))) => result as u64,
         _ => 0,
     }
+}
+
+pub fn solution_02_bf(input: &str) -> u64 {
+    let lines = input.split("\n").collect::<Vec<_>>();
+    let directions = lines[0].trim();
+
+    let parsed = lines
+        .iter()
+        .skip(2)
+        .map(|line| line.replace(" = (", " ").replace(",", "").replace(")", ""))
+        .collect::<Vec<_>>();
+    let nodes = create_nodes(parsed.iter().map(String::as_str).collect());
+    let start_keys = nodes
+        .keys()
+        .copied()
+        .into_iter()
+        .filter(|key| match key.chars().last() {
+            Some('A') => true,
+            _ => false,
+        })
+        .collect::<Vec<_>>();
+
+    let start_nodes = start_keys
+        .iter()
+        .map(|key| &nodes[*key])
+        .collect::<Vec<_>>();
+
+    let times = directions.chars().cycle().try_fold(
+        Some((start_keys.clone(), start_nodes.clone(), 0usize)),
+        |state, direction| match state {
+            Some((keys, these_nodes, i)) => {
+                let ends = keys
+                    .iter()
+                    .flat_map(|key| key.chars().last())
+                    .filter(|&ch| ch == 'Z')
+                    .count();
+
+                if ends == keys.len() {
+                    ControlFlow::Break(Some((keys, these_nodes, i)))
+                } else {
+                    let next_keys = these_nodes
+                        .iter()
+                        .map(|node| node.next(&direction))
+                        .collect::<Vec<_>>();
+                    let next_nodes = next_keys.iter().map(|key| &nodes[key]).collect::<Vec<_>>();
+                    ControlFlow::Continue(Some((next_keys, next_nodes, i + 1)))
+                }
+            }
+            _ => ControlFlow::Break(None),
+        },
+    );
+
+    match times {
+        ControlFlow::Break(Some((_, _, result))) => result as u64,
+        _ => 0,
+    }
+}
+
+pub fn map_zs(start: &str, nodes: &HashMap<&str, Node>, directions: &str, n: usize) -> u64 {
+    let mut nth = 0;
+    let times = directions.chars().cycle().try_fold(
+        Some((start, &nodes[start], 0usize)),
+        |state, direction| match state {
+            Some((key, node, i)) => {
+                let next = node.next(&direction);
+                if key.chars().last().unwrap() == 'Z' {
+                    nth += 1;
+                    if nth == n {
+                        ControlFlow::Break(Some((key, node, i)))
+                    } else {
+                        ControlFlow::Continue(Some((next, &nodes[next], i + 1)))
+                    }
+                } else {
+                    ControlFlow::Continue(Some((next, &nodes[next], i + 1)))
+                }
+            }
+            _ => ControlFlow::Break(None),
+        },
+    );
+
+    match times {
+        ControlFlow::Break(Some((_, _, result))) => result as u64,
+        _ => 0,
+    }
+}
+
+pub fn solution_02(input: &str) -> u64 {
+    let lines = input.split("\n").collect::<Vec<_>>();
+    let directions = lines[0].trim();
+
+    let parsed = lines
+        .iter()
+        .skip(2)
+        .map(|line| line.replace(" = (", " ").replace(",", "").replace(")", ""))
+        .collect::<Vec<_>>();
+    let nodes = create_nodes(parsed.iter().map(String::as_str).collect());
+    let start_keys = nodes
+        .keys()
+        .copied()
+        .into_iter()
+        .filter(|key| match key.chars().last() {
+            Some('A') => true,
+            _ => false,
+        })
+        .collect::<Vec<_>>();
+
+    let iters_to_z = start_keys
+        .iter()
+        .map(|key| map_zs(key, &nodes, directions, 1))
+        .collect::<Vec<_>>();
+
+    iters_to_z.iter().fold(1, |acc, v| acc.lcm(v))
 }
--- day08/src/tests.rs
+++ day08/src/tests.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use crate::solution_01;
+    use crate::{solution_01, solution_02};
 
     #[test]
     fn example_01_1() {
@@ -30,6 +30,25 @@ ZZZ = (ZZZ, ZZZ)
 ";
 
         let result = solution_01(input);
+
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn example_02_1() {
+        let input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)
+";
+
+        let result = solution_02(input);
 
         assert_eq!(result, 6);
     }
