401b49af6ca4cd2cd0941db1c4bece04769df0ab
maksimowiczm <maksimowiczmateusz13@gmail.com>
2023-12-09 13:38:25 +0100

    Day 9 solution 1

---
+++ day09/src/lib.rs
@@ -0,0 +1,31 @@
+use crate::sequence::Sequence;
+use std::str;
+use str::FromStr;
+
+mod sequence;
+mod tests;
+
+fn parse_input<T>(input: &str) -> Vec<Vec<T>>
+where
+    T: FromStr,
+{
+    input
+        .split("\n")
+        .filter(|line| !line.trim().is_empty())
+        .map(|line| {
+            line.split(" ")
+                .flat_map(|v| v.parse::<T>())
+                .collect::<Vec<_>>()
+        })
+        .collect::<Vec<_>>()
+}
+
+pub fn solution_01(input: &str) -> i64 {
+    let lines = parse_input::<i64>(input);
+    let predictions = lines
+        .iter()
+        .map(|line| Sequence::from(line.clone()).predict())
+        .collect::<Vec<_>>();
+
+    predictions.iter().fold(0, |acc, v| acc + v)
+}
---
+++ day09/src/sequence.rs
@@ -0,0 +1,52 @@
+use std::fmt::Debug;
+use std::ops::{Add, Sub};
+
+#[derive(Debug)]
+pub struct Sequence<T>
+where
+    T: Sub<Output = T>,
+{
+    sequence: Vec<T>,
+    interpolation: Option<Box<Sequence<T>>>,
+}
+
+impl<T> Sequence<T>
+where
+    T: Sub<Output = T> + Copy + Add<Output = T> + Default,
+{
+    pub fn predict(&self) -> T {
+        match &self.interpolation {
+            Some(interpolation) => *self.sequence.last().unwrap() + interpolation.predict(),
+            None => Default::default(),
+        }
+    }
+}
+
+impl<T> From<Vec<T>> for Sequence<T>
+where
+    T: Sub<Output = T> + Copy + PartialEq + Default + Debug,
+{
+    fn from(sequence: Vec<T>) -> Self {
+        let mut interpolation_vec = vec![];
+        sequence
+            .iter()
+            .fold(None, |prev: Option<&T>, v| match prev {
+                Some(pv) => {
+                    interpolation_vec.push(*v - *pv);
+                    Some(v)
+                }
+                None => Some(v),
+            });
+
+        let interpolation = if sequence.iter().all(|v| *v == Default::default()) {
+            None
+        } else {
+            Some(Box::new(Sequence::from(interpolation_vec)))
+        };
+
+        Sequence {
+            sequence,
+            interpolation,
+        }
+    }
+}
